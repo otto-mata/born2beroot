c93d375459726a0f06b265b89c918d57b638e2a5
Ottomata <ottomata@proton.me>
2024-12-16 01:08:02 +0100

    Update monitoring.sh
    Update to use a homemade one instead of some random garbage

--- monitoring.sh
+++ monitoring.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-architecture=$(uname -a)
-phys_cpu_count=$(grep "cpu cores" /proc/cpuinfo | tr -d 'a-z \t:')
-threads_per_cpu=$(lscpu | grep "Thread(s)" | tr -d "A-Za-z \t:()")
-virt_cpu_count=$(( $phys_cpu_count * $threads_per_cpu ))
-mem_usage=$(free -m | awk 'NR==2{printf "%s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }')
-disk_usage=$(df -h | awk '$NF=="/"{printf "%.1f/%.1fGB (%s)\n", $3,$2,$5 }')
-cpu_usage=$(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3}')
-netaddr=$(ip a show scope global | awk '$1 == "inet" { split($2,I,"/"); print I[1] }')
-macaddr=$(ip a show scope global | awk '$1 == "link/ether" { split($2,I," "); print I[1] }')
-last_boot=$(who -b | awk '$1 == "system" {print $3 " " $4}')
-lvm_usage=$(if [ $(lsblk | grep "lvm" | wc -l) -eq 0 ]; then echo no; else echo yes; fi)
-tcp_conn=$(ss -Ht state established | wc -l)
-user_log=$(users | wc -w)
-cmds=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
-wall "    #Architecture: $architecture
-    #CPU physical : $phys_cpu_count
-    #vCPU : $virt_cpu_count
-    #Memory Usage: $mem_usage
-    #Disk Usage: $disk_usage
-    #CPU load: $cpu_usage
-    #Last boot: $last_boot
-    #LVM use: $lvm_usage
-    #Connections TCP : $tcp_conn ETABLISHED
-    #Network: IP $netaddr ($macaddr)
-    #Sudo : $cmds cmd"
+arch=$(uname -a)
+phys=$(cat /proc/cpuinfo | grep "physical id" | uniq | wc -l)
+virt=$(cat /proc/cpuinfo | grep "core id" | wc -l)
+mem=$(free | awk '$1 == "Mem:" {printf("%d/%dMb "),$3 / 1024,$2 / 1024; printf("(%.2f%%)"), $3/$2*100}')
+disk=$(df | grep "^/dev/" | grep -v "/boot$" | awk '{total += $2; used += $3} END {printf "%d/%dGb (%d%%)", used/1024, total/1024/1024,  used/total * 100}')
+load=$(top -bn1 | grep '^%Cpu' | grep -E -i -o "[0-9]+\.[0-9]\s+id" | awk '{printf "%.1f%%", 100.0 - $1}')
+boot=$(uptime --since | rev | cut -c 4- | rev)
+lvm=$(lsblk | grep lvm | wc -l | awk '{print ($1 > 0) ? "yes" : "no"}')
+conn=$(lsof -ni -sTCP:ESTABLISHED | grep TCP | awk '{print substr($0, index($0, $4))}' | uniq | wc -l | awk '{if ($1 > 0) {print $1,"ESTABLISHED"} else {print "NONE"}}')
+logs=$(users | wc -w)
+net="IP $(hostname -I)($(ip a | grep ether | awk '{print $2}'))"
+cmds=$(wc -l /var/log/sudo/sudo.log | awk '{print $1, "cmd"}')
+wall "	#Architecure: $arch
+	#CPU physical : $phys
+	#vCPU : $virt
+	#Memory Usage: $mem
+	#Disk Usage: $disk
+	#CPU load: $load
+	#Last boot: $boot
+	#LVM use: $lvm
+	#Connections TCP : $conn
+	#User log: $logs
+	#Network: $net
+	#Sudo : $cmds"
